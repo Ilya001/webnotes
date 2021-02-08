a313c3eb72aa30b9373c8284baa26648bb012fae
ilya001 <ilja.sonin2018@yandex.ru>
2021-02-08 23:35:47 +0300

    fixed bug 0.3

--- webnotes-app/src/views/Note.vue
+++ webnotes-app/src/views/Note.vue
@@ -1,79 +1,38 @@
 <template>
   <div class="row">
-    <template v-if="isActive">
-      <div class="buttons-div">
-        <DxTextBox v-model:value="noteName" width="300px" @mouseover="hover" />
-        <DxButton @click="back" text="Назад" />
-        <DxButton @click="deleteNote" text="Удалить заметку" />
-      </div>
-      <DxHtmlEditor height="300px" v-model:value="noteHtml" @mouseover="hover">
-        <DxMediaResizing :enabled="true" />
-        <DxToolbar :multiline="isMultiline">
-          <DxItem format-name="undo" />
-          <DxItem format-name="redo" />
-          <DxItem format-name="separator" />
-          <DxItem :format-values="sizeValues" format-name="size" />
-          <DxItem :format-values="fontValues" format-name="font" />
-          <DxItem format-name="separator" />
-          <DxItem format-name="bold" />
-          <DxItem format-name="italic" />
-          <DxItem format-name="strike" />
-          <DxItem format-name="underline" />
-          <DxItem format-name="separator" />
-          <DxItem format-name="alignLeft" />
-          <DxItem format-name="alignCenter" />
-          <DxItem format-name="alignRight" />
-          <DxItem format-name="alignJustify" />
-          <DxItem format-name="separator" />
-          <DxItem format-name="orderedList" />
-          <DxItem format-name="bulletList" />
-          <DxItem format-name="separator" />
-          <DxItem :format-values="headerValues" format-name="header" />
-          <DxItem format-name="separator" />
-          <DxItem format-name="color" />
-          <DxItem format-name="background" />
-          <DxItem format-name="separator" />
-          <DxItem format-name="link" />
-          <DxItem format-name="image" />
-          <DxItem format-name="separator" />
-          <DxItem format-name="clear" />
-          <DxItem format-name="codeBlock" />
-          <DxItem format-name="blockquote" />
-          <DxItem format-name="separator" />
-          <DxItem format-name="insertTable" />
-          <DxItem format-name="deleteTable" />
-          <DxItem format-name="insertRowAbove" />
-          <DxItem format-name="insertRowBelow" />
-          <DxItem format-name="deleteRow" />
-          <DxItem format-name="insertColumnLeft" />
-          <DxItem format-name="insertColumnRight" />
-          <DxItem format-name="deleteColumn" />
-        </DxToolbar>
-      </DxHtmlEditor>
-      <div class="options">
-        <div class="caption">Настройки</div>
-        <div class="option">
-          <DxCheckBox
-            v-model:value="isMultiline"
-            text="Многострочный toolbar"
-          />
-        </div>
-      </div>
-    </template>
-    <template v-else>
-      <div class="preloader"></div>
-    </template>
+    <div class="buttons-div">
+      <DxTextBox v-model:value="noteName" width="300px" />
+      <DxButton @click="back" text="Назад" />
+      <DxButton @click="deleteNote" text="Удалить заметку" />
+    </div>
+    <DxHtmlEditor v-model:value="noteHtml" :height="300">
+      <DxToolbar>
+        <DxItem format-name="undo" />
+        <DxItem format-name="redo" />
+        <DxItem format-name="separator" />
+        <DxItem :format-values="sizeValues" format-name="size" />
+        <DxItem :format-values="fontValues" format-name="font" />
+        <DxItem :format-values="headerValues" format-name="header" />
+        <DxItem format-name="separator" />
+        <DxItem format-name="bold" />
+        <DxItem format-name="italic" />
+        <DxItem format-name="strike" />
+        <DxItem format-name="underline" />
+        <DxItem format-name="separator" />
+        <DxItem format-name="alignLeft" />
+        <DxItem format-name="alignCenter" />
+        <DxItem format-name="alignRight" />
+        <DxItem format-name="alignJustify" />
+        <DxItem format-name="separator" />
+        <DxItem format-name="color" />
+        <DxItem format-name="background" />
+      </DxToolbar>
+    </DxHtmlEditor>
   </div>
 </template>
 
 <script>
-import {
-  DxHtmlEditor,
-  DxToolbar,
-  DxMediaResizing,
-  DxItem,
-} from "devextreme-vue/html-editor";
-import { DxCheckBox } from "devextreme-vue/check-box";
+import { DxHtmlEditor, DxToolbar, DxItem } from "devextreme-vue/html-editor";
 import DxButton from "devextreme-vue/button";
 import DxTextBox from "devextreme-vue/text-box";
 
@@ -81,60 +40,10 @@ export default {
   name: "Note",
   components: {
     DxHtmlEditor,
-    DxMediaResizing,
     DxToolbar,
     DxItem,
-    DxCheckBox,
     DxButton,
     DxTextBox,
-  },
-  created: function () {
-    let token = localStorage.getItem("token");
-    if (token == null) {
-      this.$router.push("/home/auth");
-    } else {
-      this.getNote();
-    }
-  },
-  watch: {
-    noteName: function (name, oldName) {
-      if (this.onHover && name != oldName) {
-        this.$fetch("/api/notes/update_note/", {
-          method: "POST",
-          body: JSON.stringify({
-            params: [this.token, this.id, this.noteName, this.noteHtml],
-          }),
-        })
-          .then((response) => {
-            if (!response.ok) throw Error(response.statusText);
-            return response.json();
-          })
-          .then((data) => {
-            if (data.result[0] != true) {
-              throw Error(data)
-            }
-          });
-      }
-    },
-    noteHtml: function (html, oldHtml) {
-      if (this.onHover && html != oldHtml) {
-        this.$fetch("/api/notes/update_note/", {
-          method: "POST",
-          body: JSON.stringify({
-            params: [this.token, this.id, this.noteName, this.noteHtml],
-          }),
-        })
-          .then((response) => {
-            if (!response.ok) throw Error(response.statusText);
-            return response.json();
-          })
-          .then((data) => {
-            if (data.result[0] != true) {
-              throw Error(data)
-            }
-          });
-      }
-    },
   },
   data() {
     return {
@@ -150,60 +59,81 @@ export default {
         "Verdana",
       ],
       headerValues: [false, 1, 2, 3, 4, 5],
-      isMultiline: true,
       token: localStorage.getItem("token"),
-      id: Number(this.$route.params.id),
-      isActive: false,
-      onHover: false,
+      noteId: Number(this.$route.params.id),
       noteName: null,
       noteHtml: null,
     };
   },
-  methods: {
-    hover() {
-      this.onHover = !this.onHover;
-    },
-    back() {
-      this.$router.push("/home");
-    },
-    getNote() {
-      setInterval(() => {
-        if (this.onHover == false) {
-          this.$fetch("/api/notes/get_notes/", {
-            method: "POST",
-            body: JSON.stringify({
-              params: [this.token, this.id],
-            }),
-          })
-            .then((response) => {
-              if (!response.ok) throw Error(response.statusText);
-              return response.json();
-            })
-            .then((data) => {
-              if (this.noteName != null && this.noteHtml) {
-                if (this.noteName != data.result[0].name) {
-                  this.noteName = data.result[0].name;
-                }
-                if (this.noteHtml != data.result[0].text) {
-                  this.noteHtml = data.result[0].text;
-                }
-              } else {
-                this.noteName = data.result[0].name;
-                this.noteHtml = data.result[0].text;
-              }
-            });
-          if (this.noteName != null && this.noteHtml != null) {
-            this.isActive = true;
-          }
+  created() {
+    let token = localStorage.getItem("token");
+    if (token == null) {
+      this.$router.push("/home/auth");
+    }
+  },
+  mounted() {
+    this.$fetch("/api/notes/get_notes/", {
+      method: "POST",
+      body: JSON.stringify({
+        params: [this.token, this.noteId],
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        if (data.result[0] != null) {
+          this.noteName = data.result[0].name;
+          this.noteHtml = data.result[0].text;
         }
-      }, 500);
+      });
+  },
+  watch: {
+    noteName() {
+      this.$fetch("/api/notes/update_note/", {
+          method: "POST",
+          body: JSON.stringify({
+            params: [this.token, this.noteId, this.noteName, this.noteHtml],
+          }),
+        })
+          .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+          })
+          .then((data) => {
+              if (data.result[0] != true){
+                  throw Error(data)
+              }
+          })
+    },
+    noteHtml() {
+      this.$fetch("/api/notes/update_note/", {
+          method: "POST",
+          body: JSON.stringify({
+            params: [this.token, this.noteId, this.noteName, this.noteHtml],
+          }),
+        })
+          .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+          })
+          .then((data) => {
+              if (data.result[0] != true){
+                  throw Error(data)
+              }
+          })
+    },
+  },
+  methods: {
+    back() {
+      this.$router.push("/home/");
     },
     deleteNote() {
-      this.getter = false;
       this.$fetch("/api/notes/delete_note/", {
         method: "POST",
         body: JSON.stringify({
-          params: [this.token, this.id],
+          params: [this.token, this.noteId],
         }),
       })
         .then((response) => {
@@ -223,30 +153,6 @@ export default {
 </script>
 
 <style scoped>
-.dx-htmleditor-content img {
-  vertical-align: middle;
-  padding-right: 10px;
-}
-
-.dx-htmleditor-content table {
-  width: 50%;
-}
-
-.options {
-  padding: 20px;
-  background-color: rgba(191, 191, 191, 0.15);
-  margin-top: 20px;
-}
-
-.caption {
-  font-size: 18px;
-  font-weight: 500;
-}
-
-.option {
-  margin-top: 10px;
-}
-
 .buttons-div {
   margin-bottom: 1.5em;
   display: flex;
